cefab1e6c7b9f92d8b027f013da4e9d3a36a4b33
ellaj19 <ellaj19@nycstudents.net>
2025-09-11 10:32:27 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+""" x = 3
+y = float(3)
+print(x,y)
+
+values = [1,2.23,5,7,2,30,15]
+print(values[6])
+ """
+
+days_of_the_week = input("what's your favorite ice cream flavor?")
+if days_of_the_week == "coffee":
+    print("yes")
+else:
+    print("incorrect")
+
+answer1 = input(days_of_the_week) coffee
