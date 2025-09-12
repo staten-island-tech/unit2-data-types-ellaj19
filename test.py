e8ca65d188016addada72543b74fbeedda6f1acf
ellaj19 <ellaj19@nycstudents.net>
2025-09-12 10:15:58 -0400

    Update test.py

--- test.py
+++ test.py
@@ -6,10 +6,24 @@ values = [1,2.23,5,7,2,30,15]
 print(values[6])
  """
 
-days_of_the_week = input("what's your favorite ice cream flavor?")
-if days_of_the_week == "coffee":
+answer1 = input("what's your name?")
+input("hello")
+answer4 = input("would you like to answer a quick servey? yes or no?")
+if answer4 == ("yes","ok","okay"):
+    print("great!")
+else:
+    print("too bad you don't get a choice")
+
+answer2 = input("what's your favorite ice cream flavor?")
+if answer2 == "coffee":
     print("yes")
 else:
     print("incorrect")
 
-answer1 = input(days_of_the_week) coffee
+answer3 = input("what is the worst flavor of ice cream?")
+if answer3 == " coffee":
+    print("you little liar everyone loves coffee")
+else:
+    print("spared; congratulations. you have great taste.")
+
+
