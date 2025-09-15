dd42d60889b879f624f3b1af62bd0df3a5de2742
ellaj19 <ellaj19@nycstudents.net>
2025-09-15 10:17:14 -0400

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ values = [1,2.23,5,7,2,30,15]
 print(values[6])
  """
 
-answer1 = input("what's your name?")
+""" answer1 = input("what's your name?")
 input("hello")
 answer4 = input("would you like to answer a quick servey? yes or no?")
 correct_answers = ["yes", "ok","sure","okay"]
@@ -23,8 +23,19 @@ else:
 
 answer3 = input("what is the worst flavor of ice cream?")
 if answer3 == "coffee":
-    print("you little liar everyone loves coffee")
+    print("you little liar everyone loves coffee. youre going to get tickled")
 else:
     print("spared; congratulations. you have great taste.")
 
+print("okay, next survey")
+ """
+answer5 = input("what's your name?")
+answer6 = input("what's your favorite day of the week?")
+if answer6 == "saturday" or answer6 == "Saturday":
+    print("")
+else:
+    print("how does it feel to be wrong?")
+answer7 = input("what's your favorite number?")
+answer8 = input("what's your favorite animal?")
 
+print(f"{answer5} was casually skipping down the streets, merry as ever on a fine {answer6}. Little did {answer5} know that they were going to get ambushed by a pack of rats. as they tried to shoo them away, {answer7} rats pounced on them and started breakdancing around her, seemingly beckoning them into their cult. Just as they thought all hope was lost, the {answer8} king appeared, summoning a gang of birds to excrete on the rats. Eventually, {answer5} left unscathed, but had ptsd from the {answer8}s and had to get therapy for their schizophrenia that made them hallucinate about becoming one witht the rats. Thus, inevitably, {answer5} now lives in the sewers and commands a rat army to pounce on other unsuspecting strangers, creating a loop of demise and terror.")
